3ae5c3f9701a5b7329ee81578173e2de9e8a46af
Mika Bibas <mikabibas@gmail.com>
2022-12-06 14:06:14 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Robot from "./components/Robot";
+import Search from "./components/Search";
+import "./App.css";
+import "tachyons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      text: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ robots: data });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  handleSearch = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
+  render() {
+    const { robots, text } = this.state;
+
+    const filterRobots = robots.filter((item) => {
+      return item.name.toLowerCase().includes(text.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <Search handleSearch={this.handleSearch} />
+        {filterRobots.map((item) => {
+          return <Robot robot={item} key={item.id} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,8 @@
+const Search = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.handleSearch} />
+    </div>
+  );
+};
+export default Search;
